f78b6fe434e8470a5a428dfcfd91b70c192b4eb2
klugjo <klughertz.j@gmail.com>
2018-09-20 19:17:28 +0800

    Add the node boilerplate

---
+++ server/docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "express-rest-es2017-boilerplate",  "version": "1.2.2",  "description": "Express boilerplate for building RESTful APIs",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-09-19T14:01:01.596Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});

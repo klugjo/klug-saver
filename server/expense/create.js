27d2a9976684a4f80636551ee397ea81c3e8bfc8
klugjo <klughertz.j@gmail.com>
2018-10-28 18:40:34 +0800

    Server setup

---
+++ server/expense/create.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  const amount = parseInt(data.amount);
+  if (isNaN(amount)) {
+    console.error('Cannot parse the amount');
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the expense item. Amount not a valid number.',
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE_EXPENSE,
+    Item: {
+      id: uuid.v1(),
+      amount: amount,
+      category: data.category || 'None',
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  // write the expense to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the expense item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};

f4a0fa7bf9f1607521d04a2eacdfadd335134f05
klugjo <klughertz.j@gmail.com>
2019-03-26 19:00:42 +0800

    tutorial first version

---
+++ app/src/containers/Welcome/screens/Tutorial.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { KSButton } from '../../../components';
+import { IThemeConstants } from '../../../typings';
+import { withTheme } from '../../ThemeProvider/withTheme';
+
+export interface IHelloProps {
+  goNext: () => {};
+  theme: IThemeConstants;
+};
+
+const HelloBase = ({ goNext, theme }: IHelloProps) => {
+  return <View style={styles(theme).root}>
+    <Text>Tutorial</Text>
+    <KSButton text="Continue" onPress={goNext} />
+  </View>;
+};
+
+export default withTheme(HelloBase);
+
+const styles = (theme: IThemeConstants) => StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: theme.backgroundMainColor
+  }
+});
